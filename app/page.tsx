8127e17f6fd094e18ac25f4bf358aa76276de9fe
NaderElhaj <naderelhaj@isitc.u-sousse.tn>
2023-09-28 19:45:07 +0200

    getting rid of canstant

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,6 @@
 import TagInput from "@/components/TagInput";
 
 export default function Home() {
-  const handleTagAdded = (tag: string) => {
-    // Implement adding the tag or operation to your state or calculation logic.
-    // You can use React Query or other state management.
-    console.log("Added:", tag);
-  };
-
   return (
     <div>
       <TagInput />
